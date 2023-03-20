9822c0054aa9cec1d4794aeff25e1cff4eeec2c0
tmba123 <43762537+tmba123@users.noreply.github.com>
2023-03-20 13:52:00 +0000

    Comentar código páginas Inventory, Movements.

--- Pages/Inventory.cshtml.cs
+++ Pages/Inventory.cshtml.cs
@@ -6,31 +6,45 @@ namespace Lost_Videogames.Pages
 {
     public class InventoryModel : PageModel
     {
-        public string errorMessage = "";
-        
+        public string errorMessage = ""; //Variável para apresentação de erros na pagina .cshtm
 
-        public List<Game> GamesEnabled = new List<Game>();
-        public List<Warehouse> WarehousesEnabled = new List<Warehouse>();
+
+        public List<Game> GamesEnabled = new List<Game>(); //Lista de Games
+        public List<Warehouse> WarehousesEnabled = new List<Warehouse>(); //Lista de Warehouses
 
         [BindProperty]
-        public IEnumerable<Inventory> Inventories { get; set; }
+        public IEnumerable<Inventory> Inventories { get; set; } //IEnumerable para lista de Inventory
         public void OnGet()
         {
-            LostGamesContext context = new LostGamesContext();
+            LostGamesContext context = new LostGamesContext(); //Context ligação entre o .Net e base de dados MySQL
+
+            //Preenche a lista Inventories com a informação presente na base de dados.
+            //Esta lista vai ser utilizada no .cshtml para mostrar a tabela de resultados ao utilizador
             Inventories = context.GetAllInventory();
 
+            //Pesquisa de Games no estado enabled
             GamesEnabled = context.SearchGames("g.state", "enabled");
+            //Pesquisa de Warehouses no estado enabled
             WarehousesEnabled = context.SearchWarehouses("state", "enabled");
         }
 
-        public void OnGet2()
+
+        //Método OnGet2 substitui o método OnGet após o OnPost para conseguir apresentar apenas
+        //os dados filtrados da pesquisa e permitir novas pesquisas.
+        //O método OnGet devolve sempre a lista de Inventory completa (GetAllInventory).
+        public void OnGet2()  
         {
+
             LostGamesContext context = new LostGamesContext();
+            //Pesquisa de Games no estado enabled
             GamesEnabled = context.SearchGames("g.state", "enabled");
+            //Pesquisa de Warehouses no estado enabled
             WarehousesEnabled = context.SearchWarehouses("state", "enabled");
         }
         public void OnPost()
         {
+            //Verifica que pelo menos um dos campos do formulário está preenchido,
+            //mensagem de erro em caso contrário
             if ( Request.Form["selectgame"] == "" &&
                  Request.Form["selectwarehouse"] == "")
             {
@@ -39,9 +53,10 @@ namespace Lost_Videogames.Pages
                 return;
 
             }
-            LostGamesContext context = new LostGamesContext();
+            LostGamesContext context = new LostGamesContext(); //Context ligação entre o .Net e base de dados MySQL
             try
             {
+                //Chama o método search para apresentar os resultados filtrados com as opções selecionadas pelo utilizador
                 this.Inventories = context.SearchInventory(Request.Form["selectgame"], Request.Form["selectwarehouse"]);
                 OnGet2();
             }
--- Pages/Movement.cshtml.cs
+++ Pages/Movement.cshtml.cs
@@ -6,31 +6,51 @@ namespace Lost_Videogames.Pages
 {
     public class MovementModel : PageModel
     {
-        public string errorMessage = "";
+        public string errorMessage = ""; //Variável para apresentação de erros na pagina .cshtm
 
-        public List<Game> Games = new List<Game>();
-        public List<Warehouse> Warehouses = new List<Warehouse>();
+        public List<Game> Games = new List<Game>(); //Lista de Games
+        public List<Warehouse> Warehouses = new List<Warehouse>(); //Lista de Warehouses
 
         [BindProperty]
-        public IEnumerable<Movement> Movements { get; set; }
+        public IEnumerable<Movement> Movements { get; set; } //IEnumerable para lista de Movements
         public void OnGet()
         {
-            LostGamesContext context = new LostGamesContext();
+            LostGamesContext context = new LostGamesContext(); //Context ligação entre o .Net e base de dados MySQL
+
+            //Preenche a lista Movements com a informação presente na base de dados.
+            //Esta lista vai ser utilizada no .cshtml para mostrar a tabela de resultados ao utilizador
             Movements = context.GetAllMovements();
+
+            //Preenche a lista Games com a informação presente na base de dados.
+            //Esta lista vai ser utilizada no .cshtml para mostrar a tabela de resultados ao utilizador
             Games = context.GetAllGames();
+
+            //Preenche a lista Warehouses com a informação presente na base de dados.
+            //Esta lista vai ser utilizada no .cshtml para mostrar a tabela de resultados ao utilizador
             Warehouses = context.GetAllWarehouses();
 
         }
 
+        //Método OnGet2 substitui o método OnGet após o OnPost para conseguir apresentar apenas
+        //os dados filtrados da pesquisa e permitir novas pesquisas.
+        //O método OnGet devolve sempre a lista de Movements completa (GetAllGetAllMovements).
         public void OnGet2()
         {
-            LostGamesContext context = new LostGamesContext();
+            LostGamesContext context = new LostGamesContext(); //Context ligação entre o .Net e base de dados MySQL
+
+            //Preenche a lista Games com a informação presente na base de dados.
+            //Esta lista vai ser utilizada no .cshtml para mostrar a tabela de resultados ao utilizador
             Games = context.GetAllGames();
+            
+            //Preenche a lista Warehouses com a informação presente na base de dados.
+            //Esta lista vai ser utilizada no .cshtml para mostrar a tabela de resultados ao utilizador
             Warehouses = context.GetAllWarehouses();
         }
 
         public void OnPost()
         {
+            //Verifica que pelo menos um dos campos do formulário está preenchido,
+            //mensagem de erro em caso contrário
             if (
                 Request.Form["selectgame"] == "" &&
                 Request.Form["selectwarehouse"] == "" &&
@@ -46,6 +66,7 @@ namespace Lost_Videogames.Pages
             LostGamesContext context = new LostGamesContext();
             try
             {
+                //Chama o método search para apresentar os resultados filtrados com as opções selecionadas pelo utilizador
                 this.Movements = context.SearchMovements(Request.Form["selectgame"], Request.Form["selectwarehouse"], Request.Form["selectmovement"], Request.Form["date"]);
                 OnGet2();
             }
